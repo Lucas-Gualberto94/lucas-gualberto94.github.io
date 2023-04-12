4f28c2e40287c5238a9f23c738e7ae1c12935bfa
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-12 11:57:52 +0100

    adding nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+// import incons
+import {BiHomeAlt, BiUser} from 'react-icons/bi';
+import {BsClipboardData, BsBriefcase, BsChatSquare } from 'react-icons/bs';
+
+//link
+import { Link } from 'react-scroll';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className='fixed bottom-2 lg:bottom-8 w-full over-flow-hidden z-50 '>
+      <div className='container mx-auto'>
+        {/* nav inner */}
+        <div className='w-full bg-black/20 h-[96px]  backdrop-2xl rounded-full 
+        max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50 '>
+          <Link>
+            <BiHomeAlt />
+          </Link>
+          <Link>
+            <BiUser />
+          </Link>
+          <Link>
+            <BsClipboardData />
+          </Link>
+          <Link>
+            <BsBriefcase />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
