bbd7c3d9a6563f107aa0438b43d899af4aa4c9af
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-16 21:08:52 +0100

    adc a botton on the top of the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+//images
+import logo from '../assets/logo.svg';
 
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+  <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify between items-center'>
+          <img src={logo} alt=''></img>
+          <button className='btn btn-sm'>Work with me</button>
+      </div>
+    </div>
+  </header>
+  );
 };
 
 export default Header;
