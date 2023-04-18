380b1f1294ca607bba64ec5826cf9e431d5d35e3
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-18 11:12:26 +0100

    updatting banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,16 +11,26 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className='container mx-auto'>
         <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* text*/}
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+            <motion.h1 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
               Lucas <span>Gualberto</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary
-            font-semibold uppercase lead-[1]'>
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn('up', 0.4)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary
+              font-semibold uppercase lead-[1]'>
               <span className='text-white mr-4'>I am a</span>
               <TypeAnimation 
                 sequence={[
@@ -36,20 +46,35 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.p 
+              variants={fadeIn('up', 0.5)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>
               Lorem Ipsum is simply dummy text of the printing and typesetting
                  industry. Lorem Ipsum has been the industry's standard dummy text 
                  ever since the 1500. 
-            </p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p>
+            <motion.div 
+              variants={fadeIn('up', 0.6)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'>Contact me</button>
               <a href='https://github.com/Lucas-Gualberto94' className='text-gradient btn-link'>
                 My Portfolio
               </a>
-            </div>
+            </motion.div>
             {/* socials */}
-            <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <motion.div 
+              variants={fadeIn('up', 0.7)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
             <a href='https://github.com/Lucas-Gualberto94'>
                 <FaGithub />
               </a>
@@ -59,12 +84,16 @@ const Banner = () => {
               <a href='https://www.instagram.com/lucas__gualberto/'>
                 <FaInstagram />
               </a>
-            </div>
+            </motion.div>
           </div>
           {/* image*/}
-          <div>
+          <motion.div 
+            variants={fadeIn('down', 0.5)} 
+            initial="hidden" 
+            whileInView={'show'} 
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto'>
             <img src={Image} alt='' />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
