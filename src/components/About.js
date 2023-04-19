646d0ec92c8d368b113d2d3b9dab793222d733f9
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-19 15:15:55 +0100

    starting about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,24 +2,24 @@ import React from 'react';
 //Countup
 //import CountUp from 'react-countup/build/CountUp';
 //intersection obsorver hooker
-import { useInView } from 'react-intersection-observer';
+//import { useInView } from 'react-intersection-observer';
 //motion
 //import { motion } from 'framer-motion';
 //variant
 //import { fadeIn} from '../variants';
 
 const About = () => {
-  const [ref, inView] = useInView({
-    threshold:0.5, 
-  })
   return (
-    <section className='section' id='about' ref={ref}>
-      <div className='container mx-auto'>
-        <div>
-          <div className='flex-1 bg-auto bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
-          </div>
-          <div>Text</div>
-        </div>
+    <section className='section' id='about' >
+      <div>
+        <p>I am a front-end developer with master's degree in Physics from the Inter-unit 
+          Science Teaching Program at University of São Paulo (PIEC- USP). A fast learner with
+          good communication skills. My current passion is to develop websites and pages.
+          My journey in programmation started when I did technical course of mechatronics in
+          National Service for Industrial Learning (SENAI). There I had contact with C, C++, 
+          Assembly and CLP. In my master I worked with data analysis using R language. Nowaday 
+          I´ve studied JavaScript using React as a framework.    
+        </p>
       </div>
     </section>
   );
