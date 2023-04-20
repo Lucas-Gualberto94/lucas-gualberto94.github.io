ee53e7a36fa4d97bbccfc45b64fb24941eff918e
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-20 11:34:30 +0100

    adjusting logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   <header className='py-8'>
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
-            <img src={logo4} alt='' />
+            <img className='object-scale-down h-48 w-96' src={logo4} alt='' />
           <button className='btn btn-sm'>Work with me</button>
       </div>
     </div>
