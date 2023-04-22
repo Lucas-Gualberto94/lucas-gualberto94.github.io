e51eb7c2a9dce887c8ca5668a38fedd3209d3b86
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-22 19:28:27 +0100

    adjusting

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import image1 from '../assets/teacher.jpeg';
 //Countup
-import CountUp from 'react-countup';
+//import CountUp from 'react-countup';
 //intersection obsorver hooker
-import { useInView } from 'framer-motion';
+//import { useInView } from 'framer-motion';
 //motion
 //import { motion } from 'framer-motion';
 //variant
-//import { fadeIn} from '../variants';
+//import { fadeIn} from '../variants'; 
+//const About = () => {
+//  const [ref, inView] = useInView({
+//    threshold: 0.5,
+//  }); ref={ref}
 
-const About = () => {
-  const [ref, inView] = useInView({
-    threshold: 0.5,
-  });
 
   return (
-    <section className='section' id='about' ref={ref}>
+    <section className='section' id='about' >
       <div className='container mx-auto'>
         <div className='flex flex-col gap-y-6 lg:flex-row lg:items-center lg:gap-x-12'>
           <div className='lg:flex flex-1 max-w-[300px] lg:max-w-[450px] mx-auto'>
@@ -30,6 +30,7 @@ const About = () => {
               Assembly and CLP. In my master I worked with data analysis using R language. Nowaday 
               I´ve studied JavaScript using React as a framework.    
             </p>
+            {/* 
             <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
@@ -65,6 +66,7 @@ const About = () => {
                 </div>
               </div>
             </div>
+            */}
           </div>
         </div>
       </div>
