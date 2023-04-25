fc14faff50788e8fce78038b8b99b5f3614042a9
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-25 11:44:03 +0100

    adding work grid

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,7 +8,47 @@ const Work = () => {
     <section className='section' id='work'>
       <h2 className='h2 leading-tight text-accent'>My recent projects</h2>
       
-      <div className='container'>
+      <div className='container grid grid-cols-1 md:grid-cols-3'>
+        <article>
+          <div>
+            <img src={img1} alt='' />
+          </div>
+          <h3>Calculator</h3>
+          <a href='https://github.com/Lucas-Gualberto94/Calculator' className='btn'
+            target='_blank'  rel="noreferrer">github</a>
+          <a href='https://lucas-gualberto94.github.io/Calculator/' className='btn btn-primary'
+            target='_blank'  rel="noreferrer">Live Demo</a>
+        </article>
+        <article>
+        <div>
+            <img src={img2} alt='' />
+          </div>
+          <h3>To do list</h3>
+            <a href='https://github.com/Lucas-Gualberto94/Lista-de-tarefas' className='btn'
+            target='_blank'  rel="noreferrer">github</a>
+            <a href='https://lucas-gualberto94.github.io/Lista-de-tarefas/' className='btn btn-primary'
+            target='_blank'  rel="noreferrer">Live Demo</a>
+        </article>
+        <article>
+          <div>
+            <img src={img1} alt='' />
+          </div>
+          <h3>Calculator</h3>
+          <a href='https://github.com/Lucas-Gualberto94/Calculator' className='btn'
+            target='_blank'  rel="noreferrer">github</a>
+          <a href='https://lucas-gualberto94.github.io/Calculator/' className='btn btn-primary'
+            target='_blank'  rel="noreferrer">Live Demo</a>
+        </article>
+        <article>
+        <div>
+            <img src={img2} alt='' />
+          </div>
+          <h3>To do list</h3>
+            <a href='https://github.com/Lucas-Gualberto94/Lista-de-tarefas' className='btn'
+            target='_blank'  rel="noreferrer">github</a>
+            <a href='https://lucas-gualberto94.github.io/Lista-de-tarefas/' className='btn btn-primary'
+            target='_blank'  rel="noreferrer">Live Demo</a>
+        </article>
         <article>
           <div>
             <img src={img1} alt='' />
