0986526f2083debdd30117cfe8be07f64a13c175
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-05-03 16:18:02 +0100

    test1

--- src/components/About.js
+++ src/components/About.js
@@ -14,16 +14,19 @@ const About = () => {
     <section className='section h-screen' id='about' ref={ref} >
       <div className='container mx-auto'>
         <div>
-        <h2 className='h2 text-accent items-center'>About me</h2>
-        </div>
-        <div className='flex flex-col gap-y-6 lg:flex-row lg:items-center lg:gap-x-20 
-        lg:gap-y-0 h-screen'>
-          <div className='lg:flex flex-1 max-w-[300px] lg:max-w-[450px] mx-auto'>
+          <div className='flex-1 max-w-[300px] lg:max-w-[450px] mix-blend-lighten bg-top'>
             <img src={image1} alt=''></img>
           </div>
-          <div className='flex-1 text-center font-secondary lg:text-left'>
-            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
-              <div>
+          <div>
+            <h2 className='h2 text-accent items-center'>About me</h2>
+            <p className='p-4 border-2 border-white/50 rounded-x1'>I am a front-end developer with 
+              <b>master's degree</b> in Physics from the Inter-unit 
+              Science Teaching Program at University of São Paulo (PIEC- USP). A fast learner with
+              good communication skills. My current passion is to develop websites and pages.
+              I had contact with C, C++, Assembly and CLP in SENAI. In my master I worked with data analysis
+              using R language. Nowaday I´ve studied JavaScript using React as a framework.    
+            </p>
+            <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
                   {inView ? <CountUp start={0} end={12} duration={3}/> : 
                   null}
@@ -45,8 +48,7 @@ const About = () => {
                   programming
                 </div>
               </div>
-              <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+              <div className='text-[40px] font-tertiary text-gradient mb-2'>
                   {inView ? <CountUp start={0} end={2} duration={3}/> : 
                   null}
                 </div>
@@ -55,15 +57,9 @@ const About = () => {
                   Exchange <br/>
                   student
                 </div>
-              </div>
-            </div>
-            <p className='p-4 border-2 border-white/50 rounded-x1'>I am a front-end developer with 
-              <b>master's degree</b> in Physics from the Inter-unit 
-              Science Teaching Program at University of São Paulo (PIEC- USP). A fast learner with
-              good communication skills. My current passion is to develop websites and pages.
-              I had contact with C, C++, Assembly and CLP in SENAI. In my master I worked with data analysis
-              using R language. Nowaday I´ve studied JavaScript using React as a framework.    
-            </p>
+              <div>
+          </div>
+            
           </div>
         </div>
       </div>
