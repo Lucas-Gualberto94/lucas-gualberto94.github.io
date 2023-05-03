7e64bbf1952e9f22300c486246f20ef56ef92829
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-05-03 11:35:15 +0100

    teste

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,16 +11,16 @@ import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <section className='h-screen flex items-center' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className='container mx-auto'>
-        <div className='flex flex-col gap-y-3 lg:flex-row lg:items-center lg:gap-x-12'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* text*/}
           <div className='flex-1 text-center font-secondary lg:text-left'>
             <motion.h1 
               variants={fadeIn('up', 0.3)} 
               initial="hidden" 
               whileInView={'show'}
-              viewport={{once: false, amount: 0.7}} 
+              viewport={{once: false, amount: 0.7 }} 
               className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
               Lucas <span>Gualberto</span>
             </motion.h1>
@@ -29,8 +29,8 @@ const Banner = () => {
               initial="hidden" 
               whileInView={'show'}
               viewport={{once: false, amount: 0.7}} 
-              className='mb-2 text-[36px] sm:text-[40px] font-secondary
-              font-semibold uppercase lead-[1]'>
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary
+              font-semibold uppercase leading-[1]'>
               <span className='text-white mr-4'>I am</span>
               <TypeAnimation 
                 sequence={[
@@ -54,7 +54,7 @@ const Banner = () => {
               initial="hidden" 
               whileInView={'show'}
               viewport={{once: false, amount: 0.7}} 
-              className='mb-2 max-w-lg mx-auto lg:mx-0'>
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>
               This page is my portfolio to promote my work on programming, science, 
               academic research and education. 
             </motion.p>
@@ -63,11 +63,20 @@ const Banner = () => {
               initial="hidden" 
               whileInView={'show'}
               viewport={{once: false, amount: 0.7}} 
-              className='flex max-w-max gap-x-6 items-center mb-2 mx-auto lg:mx-0'>
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-sm'>Contact me</button>
               <a href='https://github.com/Lucas-Gualberto94' className='text-gradient btn-link'>
                 My Portfolio
               </a>
+            </motion.div>
+
+            <motion.div
+              variants={fadeIn('up', 0.7)} 
+              initial="hidden" 
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}} 
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:max-0'
+            >
               <a href='https://github.com/Lucas-Gualberto94'>
                 <FaGithub />
               </a>
@@ -85,7 +94,7 @@ const Banner = () => {
             variants={fadeIn('down', 0.5)} 
             initial="hidden" 
             whileInView={'show'} 
-            className='hidden lg:flex flex-1 max-w-[300px] lg:max-w-[450px] mx-auto'>
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
             <img src={Image} alt='' />
           </motion.div>
         </div>
