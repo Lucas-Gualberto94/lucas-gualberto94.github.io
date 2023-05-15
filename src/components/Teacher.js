891f19c8cd4519abc4b1b32f49071c49b39120de
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-05-15 11:13:08 +0100

    structure of teacher section

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { MdCastForEducation } from "react-icons/md";
+import { Button } from "react-bootstrap";
 
 function Teacher() {
   return (
-  <section className="resume-section">
-    <h2>Lucas</h2>
-  </section>
+    <section className="resume-section">
+      <div>
+        <h2>Teacher</h2>
+        <p>This section I will share the material create by me about Physics and English</p>
+        <Button className="me-5" variant="danger" href="#" size="lg">
+          <MdCastForEducation /> Phisics
+        </Button>
+        <Button variant="success" href="#" size="lg">
+          <MdCastForEducation /> English
+        </Button>
+
+      </div>
+
+    </section>
   );
 }
 
